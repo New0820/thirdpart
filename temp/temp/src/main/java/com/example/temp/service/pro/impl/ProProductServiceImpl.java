81032c5d7ea55fef5886392264985d28393ba00b
taoqimin <tqm19960707@163.com>
2023-04-30 16:31:25 +0800

    增加非会员30天判断

--- temp/temp/src/main/java/com/example/temp/service/pro/impl/ProProductServiceImpl.java
+++ temp/temp/src/main/java/com/example/temp/service/pro/impl/ProProductServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.temp.service.pro.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.temp.constant.ConstantCommon;
 import com.example.temp.constant.ConstantNums;
+import com.example.temp.constant.ConstantRedisKey;
 import com.example.temp.entity.param.ParamSaveProduct;
 import com.example.temp.entity.pro.ProProduct;
 import com.example.temp.mapper.pro.ProProductMapper;
 import com.example.temp.service.pro.ProProductService;
+import com.example.temp.util.RedisUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -24,6 +27,9 @@ public class ProProductServiceImpl implements ProProductService {
     @Resource
     private ProProductMapper proProductMapper;
 
+    @Resource
+    private RedisUtil redisUtil;
+
     /**
      * 添加商品
      *
@@ -31,19 +37,28 @@ public class ProProductServiceImpl implements ProProductService {
      */
     @Override
     public void saveProduct(ParamSaveProduct param) {
-        if (isUpload(param.getShopId())) {
+        if (!isUpload(param.getShopId())) {
             //大于30件商品。不能入库
         }
     }
 
+    /**
+     * 判断店铺是否为30件商品 会员店铺
+     *
+     * @param shopId
+     * @return
+     */
     public Boolean isUpload(Integer shopId) {
-        QueryWrapper<ProProduct> query = new QueryWrapper();
-        query.eq("fk_shp_shop_id", shopId);
-        query.ge("fk_pro_state_code", ConstantNums.TEN);
-        query.le("fk_pro_state_code", ConstantNums.TWENTY_NINE);
-        Integer productNum = proProductMapper.selectCount(query);
-        if (productNum >= ConstantNums.THIRTY) {
-            return false;
+        String memberState = redisUtil.get(ConstantRedisKey.getMemberStateRedisKeyByShopId(shopId));
+        if (!(ConstantCommon.ZERO + ConstantCommon.ONE).contains(memberState)) {
+            QueryWrapper<ProProduct> query = new QueryWrapper<>();
+            query.eq("fk_shp_shop_id", shopId);
+            query.ge("fk_pro_state_code", ConstantNums.TEN);
+            query.le("fk_pro_state_code", ConstantNums.TWENTY_NINE);
+            Integer productNum = proProductMapper.selectCount(query);
+            if (productNum >= ConstantNums.THIRTY) {
+                return false;
+            }
         }
         return true;
     }
